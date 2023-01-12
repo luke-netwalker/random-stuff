c4e8f44df15bea27989ce0cebaa85eb7faea3f3a
Pascali <112274736+luke-netwalker@users.noreply.github.com>
2023-01-12 19:48:06 +0100

    Update api_show_routes_Checkpoint.py
    aggiunta di alcuni commenti

--- api_show_routes_Checkpoint.py
+++ api_show_routes_Checkpoint.py
@@ -38,8 +38,8 @@ sid = login(utenza,secret)
 #Estrazione delle intefacce (JSON) e ottimizzazione dell'output
 interface_result = api_call(ip, porta, 'show-interfaces', {}, sid) #se il firewall interrogato ha più di 200 rotte allora va ripetuto più volte alzando l'offset
 JInterface = {"destination": [], "nexthop": [], "interface": []}
-k=0
-y=0
+k=0 #variabile per fare da contatore utile a scorrere tutti i valori della lista degli oggetti contenuti nel json
+y=0 #variabile necessaria a contare quante interfacce sono effettivamente configurate, servirà più avanti
 for object in interface_result["objects"]:
     address = interface_result["objects"][k]["ipv4-address"]
     mask = str(interface_result["objects"][k]["ipv4-mask-length"])
@@ -57,7 +57,7 @@ for object in interface_result["objects"]:
 #Estrazione delle rotte (JSON) e ottimizzazione dell'output
 static_route_result = api_call(ip, porta, 'show-static-routes', {"limit":200, "offset":0, "order": "DESC"}, sid) #se il firewall interrogato ha più di 200 rotte allora va ripetuto più volte alzando l'offset
 JRoute = {"destination": [], "nexthop": [], "interface": []}
-i=0
+i=0 #variabile per fare da contatore utile a scorrere tutti i valori della lista degli oggetti contenuti nel json
 for object in static_route_result["objects"]:
     address = static_route_result["objects"][i]["address"]
     mask = str(static_route_result["objects"][i]["mask-length"])
@@ -65,15 +65,15 @@ for object in static_route_result["objects"]:
     next_hop = static_route_result["objects"][i]["next-hop"][0]["gateway"]
     host = IPv4Address(next_hop)
     error_Counter = 0
-    z = y
-    while z > 0:
+    z = y #assegna alla variabile z dello stesso valore di interfacce utilizzate
+    while z > 0: #verifica dietro quale interfaccia si trova il next hop 
         rete = IPv4Network(JInterface["destination"][z-1], strict=False)
-        if host in rete:
+        if host in rete: 
             JRoute["interface"].append(JInterface["interface"][z-1])
         else:
             error_Counter += 1
             if error_Counter == y:
-                JRoute["interface"].append("ERRORE di Configurazione")
+                JRoute["interface"].append("ERRORE di Configurazione") #se non trova nessuna interfacca evidentemente è stata configurata male una rotta lato firewall
         z -= 1
     z = y
     JRoute["destination"].append(network)
