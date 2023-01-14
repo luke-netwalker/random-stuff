1ab99fcfb05bcec0a92b69564131e015f2629f38
Pascali <112274736+luke-netwalker@users.noreply.github.com>
2023-01-14 09:52:33 +0100

    Update XML2CSV.py
    Integrata la chiamata API

--- XML2CSV.py
+++ XML2CSV.py
@@ -2,19 +2,33 @@
 #lib
 import xml.etree.ElementTree as Xet
 import pandas as pd
-import sys
+import requests
+import urllib3
 #fine lib
 #----------------------------------------------------
+urllib3.disable_warnings() #disabilitazione warning dovuti dall'assenza di un certificato valido sul firewall
+#-----------------------------------------------------------------------------------------------------
 #variabili
 cols = ["flags", "destination", "nexthop", "descr_NH", "interface", "virtual-router"] #in questa lista si definiscono i valori dell'intestazione del file CSV
 rows = []
-Input = sys.argv[1] + '.xml'
-Output = sys.argv[1] + '.csv'
+ip = "x.x.x.x"
+REST_API_TOKEN = "xxxxxxx"
+Authorization = "Basic xxxxxxxxxxx"
+output = "Rounting_PA_" + ip + "_.csv"
 #fine variabili
 #----------------------------------------------------
+#Chiamata API
+url = "https://" + ip + "/api/?REST_API_TOKEN=" + REST_API_TOKEN + "&type=op&cmd=%3Cshow%3E%3Crouting%3E%3Croute%3E%3C%2Froute%3E%3C%2Frouting%3E%3C%2Fshow%3E"
+payload={}
+headers = {
+  'Authorization': Authorization
+}
+response = requests.request("GET", url, headers=headers, data=payload, verify=False)
+response = response.content.decode()
+#fine chiamata API
+#----------------------------------------------------
 #programma
-xmlparse = Xet.parse(Input)                                 #esegue il parse dell'informazioni presenti nel file XML che gli viene passato in "input"
-root = xmlparse.getroot()                                   #estrae i tag xml
+root = Xet.fromstring(response)                             #esegue il parse dell'informazioni presenti nel file XML che gli viene passato in "input"
 for i in root.findall('./result/entry'):                    #esegue un ciclo for per ogni tag <entry> presente nel file xml
     flags = i.find("flags").text                            #estrae il valore contenuto all'interno del tag <flags>
     destination = i.find("destination").text                #estrae il valore contenuto all'interno del tag <destination>
@@ -27,6 +41,6 @@ for i in root.findall('./result/entry'):                    #esegue un ciclo for
         "interface": interface,
         "virtual-router": virtual_router})
 df = pd.DataFrame(rows, columns=cols)                       #construisce il data frame                  
-df.to_csv(Output)                                           #onverte il data frame in CSV
+df.to_csv(output,  index = False)                           #onverte il data frame in CSV
 #fine programma
 #----------------------fine--------------------------
